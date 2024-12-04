ef0a0bc7c881f3cfb4cbdf6201a3ae2a2e7d3fce
Michal Wozniak <mickwoz1@wp.pl>
2024-12-04 22:20:40 +0100

    Dodano instrukcje do zapisania imion graczy

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function gramy(){
     let gracz1 = document.getElementById("gracz1").value;
     let gracz2 = document.getElementById("gracz2").value;
+
+    let polaGraczy = document.querySelectorAll(".gracz")
+    polaGraczy[0].value = gracz1;
+    polaGraczy[1].value = gracz2;
 }
