5271063b343dc8437f8e12739524df052442681c
Michal Wozniak <mickwoz1@wp.pl>
2024-12-06 12:22:15 +0100

    Wesja gotowa

--- script.js
+++ script.js
@@ -2,43 +2,115 @@ const przyciski = document.querySelectorAll(".gameButton");
 let g1 = document.querySelector("#g1");
 let g2 = document.querySelector("#g2");
 let gracz;
+let ruch = 0;
+let sprawdzX = new Array();
+let sprawdzO = new Array();
+const wygrana = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f'],
+    ['g', 'h', 'i'],
+    ['a', 'd', 'g'],
+    ['b', 'e', 'h'],
+    ['c', 'f', 'i'],
+    ['a', 'e', 'i'],
+    ['c', 'e', 'g']
+]
 
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
     document.querySelector("#start").addEventListener("click", gramy)
 })
 
-function gramy(){
+function gramy() {
     console.log("Wybierz gracza");
     // wprowadzamy imiona
     let graczPierwszy = document.querySelector("#gracz1").value;
     let graczDrugi = document.querySelector("#gracz2").value;
-    
+
     g1.value = graczPierwszy + " X";
     g2.value = graczDrugi + " O";
 
-    g1.addEventListener("click", function(){
-        gracz = 'X';
-        g1.setAttribute("style", "border:  2px solid black;");
-        g2.setAttribute("style", "border:  none;");
-        zaznaczGracza();
-    })
+    g1.addEventListener("click", obslugaX)
 
-    g2.addEventListener("click", function(){
-        gracz = 'O';
-        g2.setAttribute("style", "border:  2px solid black;");
-        g1.setAttribute("style", "border:  none;");
-        zaznaczGracza();
+    g2.addEventListener("click", obslugaO)
+}
+
+let obslugaX = function () {
+    gracz = 'X';
+    g1.setAttribute("style", "border:  2px solid black;");
+    g2.setAttribute("style", "border:  none;");
+    zaznaczGracza();
+}
+
+let obslugaO = function () {
+    gracz = 'O';
+    g2.setAttribute("style", "border:  2px solid black;");
+    g1.setAttribute("style", "border:  none;");
+    zaznaczGracza();
+}
+
+function sprawdz(sprawdzenie) {
+    // remis
+    if (ruch === 9) {
+        // info o remisie
+
+        setTimeout(() => {
+            document.location.reload;
+        }, 3000)
+    }
+
+    wygrana.forEach(e => {
+        if (e.every(i => sprawdzenie.includes(i))) {
+            return koniec(gracz); // wygrana i koniec gry
+        }
     })
 }
 
-function zaznaczGracza(){
-    for(let p of przyciski){
-        p.addEventListener("click", function(e){
-            if(e.target.innerText != 'X' && e.target.innerText != 'O'){
+function koniec(wygrany) {
+    let graczPierwszy = document.querySelector("#gracz1").value;
+    let graczDrugi = document.querySelector("#gracz2").value;
+
+    // pokaż kto wygra
+    // (wygrany === 'X') ? "wygral: " + graczPierwszy : "wygral: " + graczDrugi;
+
+    // dezaktywacja planszy
+    przyciski.forEach((e) => {
+        e.disabled = true;
+    })
+
+    // restart gry
+    setTimeout(() => {
+        document.location.reload;
+    }, 3000)
+}
+
+function zaznaczGracza() {
+    for (let p of przyciski) {
+        p.addEventListener("click", function (e) {
+            if (e.target.innerText != 'X' && e.target.innerText != 'O') {
                 document.getElementById(e.target.id).innerHTML = gracz; // wstawia znak
+
+                if (gracz === 'X') {
+                    sprawdzX.push(e.target.id);
+                    ruch++;
+                    sprawdz(sprawdzX);
+                    gracz = 'O';
+                    g2.setAttribute("style", "border:  2px solid black;");
+                    g1.setAttribute("style", "border:  none;");
+
+                } else if (gracz === 'O') {
+                    sprawdzO.push(e.target.id);
+                    ruch++;
+                    sprawdz(sprawdzO);
+                    gracz = 'X';
+                    g1.setAttribute("style", "border:  2px solid black;");
+                    g2.setAttribute("style", "border:  none;");
+
+                }
             }
         })
     }
+    g1.removeEventListener("click", obslugaX, true);
+    g2.removeEventListener("click", obslugaO, true);
 }
 
 /*function gramy(){
